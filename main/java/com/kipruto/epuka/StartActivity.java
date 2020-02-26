57b2afaf64404c29cb3be93544255a73ff1a2c5c
tvinnie <tvinnie77@gmail.com>
2020-02-26 12:11:44 +0300

    epuka project

---
+++ main/java/com/kipruto/epuka/StartActivity.java
@@ -0,0 +1,59 @@
+package com.kipruto.epuka;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.camera2.params.MandatoryStreamCombination;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.StringTokenizer;
+
+public class StartActivity extends AppCompatActivity {
+    Button login, register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check the user if is null
+        if(firebaseUser !=null){
+            Intent intent=new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+
+        login =findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
+
